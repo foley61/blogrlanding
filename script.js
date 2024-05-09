f2892b2a53576c66426b1f5628c997b8fc181c37
foley61 <coder61616@gmail.com>
2024-05-09 18:00:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function drop1() {
+    var x = document.querySelector("#drop")
+    x.setAttribute('class', 'textclass');
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+    x.style.marginLeft = "26.5rem"
+    x.style.flexWrap = "wrap"
+  
+
+  }
+  
+function drop2() {
+      var x = document.querySelector("#drop")
+      x.style.display = "none";
+  }
+function drop3() {
+    var x = document.querySelector("#drop1")
+    x.setAttribute('class', 'textclass');
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+    x.style.marginLeft = "33.3rem"
+    x.style.flexWrap = "wrap"
+  }
+  
+function drop4() {
+      var x = document.querySelector("#drop1")
+      x.style.display = "none";
+  }
+function drop5() {
+    var x = document.querySelector("#drop2")
+    x.setAttribute('class', 'textclass');
+    x.style.display = "flex";
+    x.style.flexDirection = "column";
+    x.style.marginLeft = "40rem"
+    x.style.flexWrap = "wrap"
+  }
+  
+  function drop6() {
+      var x = document.querySelector("#drop2")
+      x.style.display = "none";
+  }
+
